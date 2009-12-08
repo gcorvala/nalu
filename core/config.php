0ec7c63a6db8a9caaea61167dfe14ed01ac46b57
Gabriel Corvalan Cornejo <gabriel@gabriel.local>
2009-12-08 16:42:12 +0100

    Add some new class

---
+++ core/config.php
@@ -0,0 +1,32 @@
+<?
+class Config {
+	private $mysql_srv_addr;
+	private $mysql_srv_port;
+	private $mysql_srv_user;
+	private $mysql_srv_pass;
+	private $mysql_db_name;
+	private $site_name;
+	private $site_url;
+
+	public function __construct () {
+		$ini = parse_ini_file ("../config.ini", true);
+		$mysql = $ini['mysql'];
+		$this->mysql_srv_addr = $mysql['srv_addr'];
+		$this->mysql_srv_port = $mysql['srv_port'];
+		$this->mysql_srv_user = $mysql['srv_user'];
+		$this->mysql_srv_pass = $mysql['srv_pass'];
+		$this->mysql_db_name = $mysql['db_name'];
+		$site = $ini['site'];
+		$this->site_name = $site['name'];
+		$this->site_url = $site['url'];
+	}
+	
+	public function get_mysql_srv_addr () { return $this->mysql_srv_addr; }
+	public function get_mysql_srv_port () { return $this->mysql_srv_port; }
+	public function get_mysql_srv_user () { return $this->mysql_srv_user; }
+	public function get_mysql_srv_pass () { return $this->mysql_srv_pass; }
+	public function get_mysql_db_name () { return $this->mysql_db_name; }
+	public function get_site_name () { return $this->site_name; }
+	public function get_site_url () { return $this->site_url; }
+}
+?>
---
+++ core/data.php
@@ -0,0 +1,39 @@
+<?
+require_once ("config.php");
+
+class Data {
+	private $instance;
+	private $link;
+
+	private function __construct () {
+		$config = new Config ();
+		$this->link = mysql_connect ($config->get_mysql_srv_addr () . ":" . $config->get_mysql_srv_port (),
+									$config->get_mysql_srv_user (),
+									$config->get_mysql_srv_pass ());
+		if ($this->link == false)
+			throw new Exception ("Mysql connection failed! : " . mysql_error ());
+		if (mysql_select_db ($config->get_mysql_db_name (), $this->link) == false)
+			throw new Exception ("Mysql database selection failed! : " . mysql_error ());
+	}
+	
+	public static function create () {
+		if (!isset (self::$instance)) {
+			$c = __CLASS__;
+			self::$instance = new $c;
+		}
+		return self::$instance;
+	}
+	
+	public function request ($req) {
+		$result = mysql_query (req);
+		if ($result == false)
+			throw new Exception ("Mysql request failed! : " . mysql_error ());
+		return $result;
+	}
+	
+	public function __destruct () {
+		if (mysql_close ($this->link) == false)
+			throw new Exception ("Mysql disconnection failed! : " . mysql_error ());
+	}
+}
+?>
