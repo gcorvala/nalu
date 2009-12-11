d4cff18774eb5180d05eea695e6829f63cd46a17
Gabriel Corvalan Cornejo <gabriel@gabriel.local>
2009-12-11 14:05:59 +0100

    Fix some bug in Feed

--- core/feed.php
+++ core/feed.php
@@ -14,28 +14,35 @@ class Feed {
 		if (empty ($url))
 			throw new Exception ("Feed : url are not defined !");
 		$this->url = $url;
-		// TODO : si user_feed et pas dans la db -> Exception
+		// FIXME : what should we insert for user_feed name, description and link ?
 		if (ereg("^user://", $this->url))
 			$user_feed = true;
 		$data = Data::create ();
 		$req = "SELECT * FROM Feeds WHERE URL LIKE '$this->url'";
 		$result = $data->request ($req);
 		if (mysql_num_rows ($result) == 0) {
-			$this->dom = new DOMDocument ();
-			if ($this->dom->load ($this->url) == false)
-				throw new Exception ("Feed : url cannot be fetched ! : $this->url");
-			$channel = $this->dom->getElementsByTagName ("channel");
-			$channel = $channel->item (0);
-			foreach ($channel->childNodes as $child) {
-				$node = $child->nodeName;
-				$$node = $child->nodeValue;
+			if ($user_feed == true) {
+				$req = "INSERT INTO Feeds (URL)";
+				$req .= " VALUES ('$this->url')";
+				$data->request ($req);
 			}
-			$this->name = utf8_decode (addslashes ($title));
-			$this->description = utf8_decode (addslashes ($description));
-			$this->link = utf8_decode (addslashes ($link));
-			$req = "INSERT INTO Feeds (URL, Name, Description, Link)";
-			$req .= " VALUES ('$this->url', '$this->name', '$this->description', '$this->link')";
-			$result = $data->request ($req);
+			else {
+				$this->dom = new DOMDocument ();
+				if ($this->dom->load ($this->url) == false)
+					throw new Exception ("Feed : url cannot be fetched ! : $this->url");
+				$channel = $this->dom->getElementsByTagName ("channel");
+				$channel = $channel->item (0);
+				foreach ($channel->childNodes as $child) {
+					$node = $child->nodeName;
+					$$node = $child->nodeValue;
+				}
+				$this->name = utf8_decode (addslashes ($title));
+				$this->description = utf8_decode (addslashes ($description));
+				$this->link = utf8_decode (addslashes ($link));
+				$req = "INSERT INTO Feeds (URL, Name, Description, Link)";
+				$req .= " VALUES ('$this->url', '$this->name', '$this->description', '$this->link')";
+				$data->request ($req);
+			}
 		}
 		else {
 			$line = mysql_fetch_array ($result);
@@ -60,17 +67,22 @@ class Feed {
 		}
 		return $items;
 	}
-	
-	/**
-	 * This function returns the list of items that we insert in the db
-	 **/
-	public function update_items () {
+
+	public function add ($item) {
+		$data = Data::create ();
+		$req = "INSERT INTO FeedItems (URLFeed, URLItem)";
+		$req .= " VALUES ('$this->url', '" . $item->get_url () . "')";
+		$data->request ($req);
+	}
+
+	public function update () {
+		if ($user_feed == true)
+			return;
 		if (!isset ($this->dom)) {
 			$this->dom = new DOMDocument ();
 			if ($this->dom->load ($this->url) == false)
 				throw new Exception ("Feed : url cannot be fetched !");
 		}
-		$items = array ();
 		$xml_items = $this->dom->getElementsByTagName ("item");
 		$data = Data::create ();
 		foreach ($xml_items as $item) {
@@ -78,13 +90,12 @@ class Feed {
 				$node = $child->nodeName;
 				$$node = $child->nodeValue;
 			}
+			// FIXME : Select is not needed (because checked into Item)
 			$req = "SELECT * FROM Items WHERE URL LIKE '$link'";
 			$result = $data->request ($req);
 			if (mysql_num_rows ($result) == 0) {
-				$items[] = new Item ($link, $title, $pubDate, $description);
-				$req = "INSERT INTO FeedItems (URLFeed, URLItem)";
-				$req .= " VALUES ('$this->url', '$link')";
-				$data->request ($req);
+				$new_item = new Item ($link, $title, $pubDate, $description);
+				$this->add ($new_item);
 			}
 		}
 	}
