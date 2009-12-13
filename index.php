a9dbc20007fedd0adbe370ee838e5d3e6faab25b
Gabriel Corvalan Cornejo <gabriel@gabriel.local>
2009-12-13 15:02:58 +0100

    Fix API switcher

--- index.php
+++ index.php
@@ -25,43 +25,15 @@ try {
 	else {
 		$user = $auth->get_user ();
 		switch ($action) {
-			case "login":
-				$action = "home";
-				break;
-			case "logout":
-				$auth->disconnect ();
-				$action = "home";
-				break;
-			case "read":
-				$item = new Item ($_GET['item']);
-				//$user->read ($item);
-				header ('Location: ' . $item->get_url ());
-				break;
-			// unread?
-			case "add_subscription":
-				$feed = new Feed ($_GET['feed']);
-				$user->subscribe_to_feed ($feed);
-				break;
-			case "remove_unsubscription":
-				$feed = new Feed ($_GET['feed']);
-				$user->unsubscribe_to_feed ($feed);
-				break;
-			case "add_friend":
-				$friend = new User ($_GET['friend']);
-				$user->add_friend ($friend);
-				break;
-			case "remove_friend":
-				$friend = new User ($_GET['friend']);
-				$user->remove_friend ($friend);
-				break;
 			case "add_comment":
 				$feed = new Feed ($_GET['feed']);
 				$item = new Item ($_GET['item']);
 				$comment = $_GET['comment'];
 				$user->add_comment ($feed, $item, $comment);
 				break;
-			case "remove_comment":
-				//$user->remove_comment ()
+			case "add_friend":
+				$friend = new User ($_GET['friend']);
+				$user->add_friend ($friend);
 				break;
 			case "add_share":
 				$feed = new Feed ($_GET['feed']);
@@ -69,8 +41,43 @@ try {
 				$note = $_GET['note'];
 				$user->share ($feed, $item, $note);
 				break;
+			case "add_subscription":
+				$feed = new Feed ($_GET['feed']);
+				$user->subscribe_to_feed ($feed);
+				break;
+			case "login":
+				$action = "home";
+				break;
+			case "logout":
+				$auth->disconnect ();
+				$action = "home";
+				break;
+			case "add_read":
+				$item = new Item ($_GET['item']);
+				//$user->read ($item);
+				break;
+			case "read":
+				$item = new Item ($_GET['item']);
+				//$user->add_read ($item);
+				header ('Location: ' . $item->get_url ());
+				break;
+			case "remove_read":
+				$item = new Item ($_GET['item']);
+				//$user->remove_read ($item);
+				break;
+			case "remove_comment":
+				//$user->remove_comment ()
+				break;
+			case "remove_friend":
+				$friend = new User ($_GET['friend']);
+				$user->remove_friend ($friend);
+				break;
 			case "remove_share":
 				//$user->remove_share ()
+				break;
+			case "remove_subscription":
+				$feed = new Feed ($_GET['feed']);
+				$user->unsubscribe_to_feed ($feed);
 				break;
 		}
 	}
